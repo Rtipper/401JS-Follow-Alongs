2fd4c14d760e9ff714745767b3679abe1372ed12
RYAN <RTIPPER44@GMAIL.COM>
2021-04-12 20:28:29 -0700

    4.12 in class examples

---
+++ Class 15 4.12/Recursion/link_list_ex.js
@@ -0,0 +1,32 @@
+'use strict';
+
+// VIA IN-CLASS REPL EXAMPLE
+// ADDITONAL FILES NOT INCLUDED HERE VIA REPL TO ACTUALLY WORK
+const practice = module.exports = {};
+
+const HashTable = require('./data-structures/hashtable');
+
+practice.QuestionOfTheDay = function() {
+}
+
+// STANDARD FUNCTION
+practice.iterateLinkedListIteratively = function(list) {
+  let biggest = list.head.value
+  let current = list.head;
+  while(current) {
+    // if(current.value > biggest) { biggest = current.vale }
+    biggest = current.value > biggest ? current.value : biggest;
+    current = current.next;
+  }
+
+  return biggest;
+}
+
+// RECURSIVE FUNCTION
+practice.iterateLinkedListIteratively = function(node, largest=0) {
+  if(!node) { return largest; }
+  largest = node.value > largest ? node.value : largest;
+  return practice.iterateLinkedListIteratively(node.next, largest);
+}
+
+
---
+++ Class 15 4.12/notes.md
@@ -0,0 +1,12 @@
+# LINKED LISTS
+
+### Purpose of the LL: 
+- Memory
+- Non-Continuous
+- Traverse Over & Over
+- Revisit
+
+### LL Structure
+{HEAD} --> {NODE(S)} --> {TAIL}
+
+### In-Class Review
